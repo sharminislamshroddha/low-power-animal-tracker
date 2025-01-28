11c37921dfde47f37a8c786aefc798e9a9a4910f
Sharmin Islam Shroddha <133664253+sharminislamshroddha@users.noreply.github.com>
2025-01-28 16:37:22 +0600

    Update and rename TinyZero_Accelerometer.ino to accelerometer_detection.ino

--- accelerometer_detection.ino
+++ accelerometer_detection.ino
@@ -1,3 +1,11 @@
+/*
+  Accelerometer Detection Code for TinyZero
+  -----------------------------------------
+  This code checks for the presence of the BMA250 accelerometer. 
+  If the accelerometer is detected, the onboard LED blinks rapidly 3 times. 
+  Otherwise, the LED blinks slowly 3 times to indicate failure.
+*/
+
 #include <Wire.h>
 
 #define BMA250_I2C_ADDR 0x18  // Accelerometer I2C address
